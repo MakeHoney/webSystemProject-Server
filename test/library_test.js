1fb498321845a9eecd41b5bea9f21c2ab7767761
makehoney <makehoney3@gmail.com>
2018-11-19 23:16:15 +0900

    add some user reservation test

--- test/library_test.js
+++ test/library_test.js
@@ -9,7 +9,9 @@ import assert from 'assert'
 
 // Describe tests
 describe('Central Library', () => {
-  let mocha_test, test_user = null
+  let test_user = null
+  let test_user2 = null
+  let mocha_test
 
   before(async () => {
     mocha_test = await dbConnection()
@@ -36,34 +38,55 @@ describe('Central Library', () => {
     await Seat.mount(30, 30, 30, 30)
 
     // register test user
-    try {
-      await User.register({
-        studentID: '201523483',
-        email: 'pourmonreve@ajou.ac.kr',
-        password: 'pass',
-        name: 'Byunghun'
-      })
-    } catch (err) {
-      console.error(err)
-    }
+    await User.register({
+      studentID: '201523483',
+      email: 'pourmonreve@ajou.ac.kr',
+      password: 'pass',
+      name: 'Byunghun'
+    })
+
+    await User.register({
+      studentID: '201533333',
+      email: 'makehoney3@ajou.ac.kr',
+      password: 'pass',
+      name: 'Makehoney'
+    })
 
     test_user = await User.findOne({ email: 'pourmonreve@ajou.ac.kr' })
+    test_user2 = await User.findOne({ email: 'makehoney3@ajou.ac.kr' })
   })
 
   afterEach(async () => {
+    // await User.findOneAndDelete({ email: test_user.email })
+    // await User.findOneAndDelete({ email: test_user2.email })
     await test_user.remove()
-    test_user = null
+    await test_user2.remove()
   })
 
-  it('Reserve a seat test', async () => {
+  it('유저 자리 신청 테스트', async () => {
+    // Reserve a seat
     await Seat.reserve({ studentID: test_user.studentID, seatNum: 19 })
-
+    // Load Document
     let seat = await Seat.findOne({ seatNum: 19 })
-
     let user = await User.findOne({ studentID: test_user.studentID })
       .populate('seat')
-
+    // Comparison
     assert(user.seat._id.toString() === seat._id.toString())
+  })
 
+  it('타인에 의해 이미 신청된 자리 신청 테스트', async () => {
+    // Reserve a seat
+    await Seat.reserve({ studentID: test_user.studentID, seatNum: 19 })
+
+    // When another user tries reservation on same seat -> must occurs exception
+    try {
+      await Seat.reserve({ studentID: test_user2.studentID, seatNum: 19 })
+    } catch (err) {
+      assert(err)
+    }
+
+    // User2 must have no seat.
+    let user2 = await User.findOne({ studentID: test_user2.studentID })
+    assert(!user2.seat)
   })
 })
