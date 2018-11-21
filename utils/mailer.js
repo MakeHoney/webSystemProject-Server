46ec6b3f0596863be87c0a0ab79ac4b711d22a2f
makehoney <makehoney3@gmail.com>
2018-11-21 14:02:06 +0900

    add mailer util

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,8 +1,18 @@
-import mailer from 'nodemailer'
+import nodemailer from 'nodemailer'
+import { config } from '../config'
 
-const transporter = mailer.createTransport({
-  service: 'Gmail',
-  auth: {
-
+export const mailer = {
+  transporter: nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: config.gmailUser,
+      pass: config.gmailPass
+    }
+  }),
+  mailerOption: {
+    from: this.gmailUser,
+    to: '',
+    subject: '클리카주 인증 메일입니다.',
+    text: '아래 링크를 통해 본인 인증을 해주세요!\n'
   }
-})
+}
